10b1368aa93f4c8742b46b034bf4127474c0a2e0
Anton Protopopov <antpro@basealt.ru>
2023-06-09 14:33:14 +0400

    feat: add ui forms 1,2,3,4

--- script/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -ex
-
-#git clone https://github.com/AntProBasealt/domain-manager.git /123
-chown -R test:test /app/
-cd /app/ && gear-rpm -ba
--- src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char ** argv)
-{
-    std::cout << "This is libdomain!" << std::endl;
-    return 0;
-}
