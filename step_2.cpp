9b183dbd7b25828493d1bc6ccef27117538022f4
Anton Protopopov <antpro@basealt.ru>
2023-06-13 13:14:21 +0400

    feat: add frame for steps, .h/.cpp

---
+++ step_2.cpp
@@ -0,0 +1,17 @@
+#include "step_2.h"
+
+Step3Page::Step3Page(){}
+
+Step3Page::~Step3Page(){}
+
+// TODO
+void Step3Page::setDcIpAddress(){}
+
+void Step3Page::setIpMask(){}
+
+void Step3Page::setDnsServer(){}
+
+void Step3Page::setGateway(){}
+
+void Step3Page::setDnsBackend(){}
+
--- step_2.h
+++ step_2.h
@@ -1,4 +1,27 @@
 #ifndef STEP_2_H
 #define STEP_2_H
 
+#include <QWidget>
+
+class Step3Page : public QWidget
+{
+    Q_OBJECT
+public:
+    Step3Page();
+    virtual ~Step3Page();
+
+private:
+    void setDcIpAddress();
+    void setIpMask();
+    void setDnsServer();
+    void setGateway();
+    void setDnsBackend();
+
+private:
+    Step3Page(const Step3Page &) = delete;
+    Step3Page(Step3Page &&)      = delete;
+    Step3Page &operator=(const Step3Page &) = delete;
+    Step3Page &operator=(Step3Page &&) = delete;
+};
+
 #endif // STEP_2_H
